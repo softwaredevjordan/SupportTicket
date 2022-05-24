cd8321fb33b2e950d4d17d32f2ccb18dc935d5bb
jordan <softwaredevjordan@protonmail.com>
2022-05-24 06:24:39 -0500

    ahhh

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   });
 } else {
   app.get("/", (req, res) => {
-    res.status(200).json("Welcome ");
+    res.status(200).json("Not deployed correctly");
   });
 }
 
