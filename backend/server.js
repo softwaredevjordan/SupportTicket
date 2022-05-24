3b348e0c0aa6837759fa52d045b6f366d8250885
jordan <softwaredevjordan@protonmail.com>
2022-05-24 06:15:10 -0500

    uggg

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,9 @@ app.use("/api/tickets", require("./routes/ticketRoutes"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
-  );
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.status(200).json("Welcome ");
