0cb8aa1ff3f750bab1c24f3111eae62dde5f6929
jordan <softwaredevjordan@protonmail.com>
2022-05-24 06:44:59 -0500

    frustrated

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200).json("Welcome ");
-});
-
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
